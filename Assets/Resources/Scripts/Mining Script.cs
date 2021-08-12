087c714d0b33b49f16e536e6ab022018c672fcd0
soupy <omniplex4@gmail.com>
2021-08-12 05:05:38 +0100

    Update Mining Script.cs

--- Assets/Resources/Scripts/Mining Script.cs
+++ Assets/Resources/Scripts/Mining Script.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MiningScript : MonoBehaviour
 {
+    public Vector3 clickPosition;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {    
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Plane plane = new Plane(Vector3.up, 0);
+
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            clickPosition = ray.GetPoint(distance);
+        }
     }
 }
